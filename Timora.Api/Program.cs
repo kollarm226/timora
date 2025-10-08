d97e51695513f500e9684db321c9c388e27d42c7
Matej Kollár <matej.kollar@kosickaakademia.sk>
2025-10-08 09:54:27 +0200

    Updated connection string and created a service extension for EFCore

--- Timora.Api/Program.cs
+++ Timora.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerServices();
+builder.Services.AddEntityFrameworkServices(builder.Configuration);
 
 var app = builder.Build();
 
