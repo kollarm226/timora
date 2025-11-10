94617973ea58a10e260da199f67c2d245a3b3f88
Matej Kollár <matej.kollar@kosickaakademia.sk>
2025-11-10 09:22:25 +0100

    Created DTOs

---
+++ Timora.Api/DTOs/CreateNoticeDto.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Timora.Api.DTOs
+{
+    /// <summary>
+    /// Data transfer object for creating a new notice.
+    /// </summary>
+    public class CreateNoticeDto
+    {
+        /// <summary>
+        /// Gets or sets the ID of the user creating the notice.
+        /// </summary>
+        [Required]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the notice.
+        /// </summary>
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the content of the notice.
+        /// </summary>
+        [Required]
+        [MaxLength(2000)]
+        public string Content { get; set; } = null!;
+    }
+}
---
+++ Timora.Api/DTOs/CreateUserDto.cs
@@ -0,0 +1,58 @@
+using System.ComponentModel.DataAnnotations;
+using Timora.Data.Entities;
+
+namespace Timora.Api.DTOs
+{
+    /// <summary>
+    /// Data transfer object for creating a new user.
+    /// </summary>
+    public class CreateUserDto
+    {
+        /// <summary>
+        /// Gets or sets the Firebase UID for the user.
+        /// </summary>
+        [Required]
+        [MaxLength(128)]
+        public string FirebaseId { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the company ID the user belongs to.
+        /// </summary>
+        [Required]
+        public int CompanyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's first name.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the user's last name.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the user's email address.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the user's username.
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the user's role. Defaults to Employee.
+        /// </summary>
+        public UserRole Role { get; set; } = UserRole.Employee;
+    }
+}
